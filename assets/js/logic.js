4206ae9d2dec6d7b77a22e19ef77bfb0444be45e
Elena Papanikolas <epapanikolas@hotmail.com>
2024-05-10 18:54:55 -0600

    Set dark and light themes to local storage.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,9 @@
 
 const themebutton = document.querySelector('.theme-button'); // getting access to the button to be able to change themes
 const body = document.querySelector('body'); // getting access to the body
-const alien =document.querySelector('.alien'); // getting access to the alien button
+const alien = document.querySelector('.alien'); // getting access to the alien button
+const link = document.querySelector('.light-link'); // getting access to portfolio link 
+
 
 let totalPosts = JSON.parse(localStorage.getItem('totalPosts')) || []; // globally declaring the array
 function storeBlogPost(blogPost) {  // creating a function to store blog posts in local storage
@@ -13,15 +15,47 @@ function getBlogPosts() {      // creating function to retrieve blog posts from 
     JSON.parse(localStorage.getItem('totalPosts')) || [];
 }    
 
+
+
+function setDarkMode() {
+    body.setAttribute('class', 'dark-mode');
+    alien.setAttribute('src', 'assets/images/aliendark.png');
+    link.setAttribute('class', 'dark-link');
+}
+
+function setLightMode() {
+    body.setAttribute('class', 'light-mode');
+    alien.setAttribute('src', 'assets/images/alienlight.png');
+    link.setAttribute('class', 'light-link');
+}
+
 let mode = 'light';
+
 themebutton.addEventListener('click', function() {  // event listener that listens for a click, and changes light and dark mode
     if (mode === 'light') {
         mode = 'dark';
-        body.setAttribute('class', 'dark-mode');
-        alien.setAttribute('src', 'assets/images/aliendark.png');
+        setDarkMode();
+        localStorage.setItem('storedMode', mode);
     } else {
         mode = 'light';
-        body.setAttribute('class', 'light-mode');
-        alien.setAttribute('src', 'assets/images/alienlight.png');
+        setLightMode();
+        localStorage.setItem('storedMode', mode);
     }
 });
+
+function saveMode() {
+    let storedMode = localStorage.getItem('storedMode');
+    if (storedMode !== null) {
+        mode = storedMode;
+    } else {
+        localStorage.setItem('storedMode', mode);
+    }
+
+    if (mode === 'light') {
+        setLightMode()
+    } else {
+        setDarkMode()
+    }
+ }
+
+saveMode();
